e54812d5a6a26c8a1ca07a752b83e521e297cb82
enzo bourgoin <enzobourgoin@air-de-enzo.home>
2021-10-29 10:48:28 +0200

    resolve comments

--- src/routes.php
+++ src/routes.php
@@ -8,7 +8,7 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['UserController', 'connect',],
-    'register' => ['UserController', 'register',],
+    'inscription' => ['UserController', 'register',],
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],
