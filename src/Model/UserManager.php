c96a21ea7c82bed4623158a703711e0238b66357
enzo bourgoin <enzobourgoin@air-de-enzo.home>
2021-11-04 14:16:58 +0100

    make back register form

---
+++ src/Model/UserManager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Model;
+
+use App\Model\AbstractManager;
+
+class UserManager extends AbstractManager
+{
+    public const TABLE = 'user';
+
+    public function create(array $userData)
+    {
+        $statement = $this->pdo->prepare('
+        INSERT INTO user (firstname, lastname, mail, password, created_at, is_admin)
+        VALUES (:firstname, :lastname, :mail, :password, NOW(), false)
+        ');
+        $statement->bindValue(':firstname', $userData['firstname'], \PDO::PARAM_STR);
+        $statement->bindValue(':lastname', $userData['lastname'], \PDO::PARAM_STR);
+        $statement->bindValue(':mail', $userData['InputEmail1'], \PDO::PARAM_STR);
+        $statement->bindValue(':password', $userData['InputPassword1'], \PDO::PARAM_STR);
+        $statement->execute();
+    }
+    public function selectOneByEmail(string $mail)
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE mail=:mail");
+        $statement->bindValue('mail', $mail, \PDO::PARAM_STR);
+        $statement->execute();
+
+        return $statement->fetch();
+    }
+}
