1e8a92eb7c592d1071b52aa4646d82a2e2023915
Stéphanie AMANS <stephanie-amans_student2021@wilder.school>
2021-11-04 09:51:01 +0100

    add function for connection

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Model\UserManager;
+
 class UserController extends AbstractController
 {
     public function register(): string
@@ -10,6 +12,23 @@ class UserController extends AbstractController
     }
     public function connect(): string
     {
-        return $this->twig->render('User/formConnect.html.twig');
+        $error = "";
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $userManager = new UserManager();
+            $userData = $userManager->selectOneByEmail($_POST['InputEmail1']);
+            if ($userData !== false) {
+                if (password_verify($_POST['InputPassword1'], $userData['password'])) {
+                    $_SESSION['user'] = $userData;
+                } else {
+                    $error = 'Vos identifiants sont incorrects';
+                }
+            } else {
+                $error = 'Vos identifiants sont incorrects';
+            }
+        }
+        return $this->twig->render('User/formConnect.html.twig', [
+                'session' => $_SESSION,
+                'error' => $error,
+                ]);
     }
 }
