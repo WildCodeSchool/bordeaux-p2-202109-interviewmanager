78fc6b6556e75c9636adc464a791ec6ff99f1394
Sebastien Juchet <sebastien.juchet@gmail.com>
2021-11-09 12:02:52 +0100

    Create admin form for add company as recommendated

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,10 +2,42 @@
 
 namespace App\Controller;
 
+use App\Model\CompanyManager;
+
 class DashboardController extends AbstractController
 {
-    public function index()
+    public function index(): string
     {
-        return $this->twig->render('Admin/index.html.twig');
+        $userId = $_SESSION['user']['id'];
+        $companyManager = new CompanyManager();
+        $errors = [];
+        $success = '';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $_POST['user_id'] = $userId;
+            $_POST['is_recommendating'] = true;
+            if (empty($_POST['name'])) {
+                $errors[] = 'Le champs ne doit pas être vide';
+            } else {
+                $success = 'Entreprise bien enregistrée';
+            }
+            $queryString = [
+                'errors' => $errors,
+                'success' => $success];
+            $companyManager->insert($_POST);
+            header('Location: admin?' . http_build_query($queryString));
+        }
+        if (!empty($_GET['errors'])) {
+            $errors['erreur'] = $_GET['errors'];
+        }
+        if (!empty($_GET['success'])) {
+            $success = $_GET['success'];
+        }
+        $companyManager = new CompanyManager();
+        $companiesRecommendating = $companyManager->countAllRecommending();
+        return $this->twig->render('Admin/index.html.twig', [
+            'companies_recommendating' => $companiesRecommendating,
+            'errors' => $errors,
+            'success' => $success,
+        ]);
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -18,5 +18,5 @@ return [
     'logout' => ['UserController', 'logout'],
     'ajoutEntreprise' => ['CompanyController', 'addCompany', ['id']],
     'entreprise' => ['CompanyController', 'show', ['id']],
-    'admin/index' => ['DashboardController', 'index'],
+    'admin' => ['DashboardController', 'index'],
 ];
