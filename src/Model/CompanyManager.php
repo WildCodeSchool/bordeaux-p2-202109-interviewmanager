89fb1b89ce202aedd09844be6a2f6e3abc978804
enzo bourgoin <enzobourgoin@air-de-enzo.home>
2021-11-10 09:51:19 +0100

    resolve errors in profil

--- src/Model/CompanyManager.php
+++ src/Model/CompanyManager.php
@@ -77,6 +77,8 @@ class CompanyManager extends AbstractManager
         $statement = $this->pdo->prepare("SELECT name FROM company WHERE user_id=:id AND is_recommendating=true");
         $statement->bindValue(':id', $id, \PDO::PARAM_INT);
         $statement->execute();
+
+        return $statement->fetchAll();
     }
     public function allRecommending(): array
     {
