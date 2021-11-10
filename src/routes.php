c89f9aaafe5cbae50aafbcfdf245064dc676726e
SebastienJuchet <60229676+SebastienJuchet@users.noreply.github.com>
2021-11-10 09:20:16 +0100

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -14,7 +14,7 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
-    'accueil' => ['CompanyController', 'index', ['id']],
+    'accueil' => ['UserController', 'index', ['id']],
     'logout' => ['UserController', 'logout'],
     'ajoutEntreprise' => ['CompanyController', 'addCompany', ['id']],
     'entreprise' => ['CompanyController', 'show', ['id']],
