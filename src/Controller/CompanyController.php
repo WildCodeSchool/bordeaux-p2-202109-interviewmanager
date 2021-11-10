184afda9484ac11b22ea101811ed7652e8d08c98
enzo bourgoin <enzobourgoin@air-de-enzo.home>
2021-11-10 16:34:25 +0100

    ajout d'un bouton delete dans la liste des entreprises du profil2

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -60,7 +60,7 @@ class CompanyController extends AbstractController
             ]);
     }
 
-    public function delete(int $id): void
+    public function delete(): void
     {
         $companyManager = new CompanyManager();
         $companyManager->delete($_GET['id']);
